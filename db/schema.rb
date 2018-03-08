21429788638ebbfc0a7f787cf5085ec28514aba1
cwy007 <chanweiyan007@gmail.com>
2018-03-08 21:02:11 +0800

    Hook teacher & course relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308124200) do
+ActiveRecord::Schema.define(version: 20180308125823) do
 
   create_table "course_relationships", force: :cascade do |t|
     t.integer "student_id"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20180308124200) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "teacher_id"
+    t.index ["teacher_id"], name: "index_courses_on_teacher_id"
   end
 
   create_table "students", force: :cascade do |t|
